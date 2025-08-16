745188630eeb0b54e79f488d7ead4f6a8d4cfe7f
Flavio Lima <ctrlserr@gmail.com>
2025-08-16 12:11:48 -0300

    dasd1231weasda

--- docker-entrypoint.d/99-env.sh
+++ docker-entrypoint.d/99-env.sh
@@ -4,5 +4,6 @@ set -e
 : "${VITE_API_URL:=}"
 
 if [ -f /usr/share/nginx/html/env.js.tpl ]; then
-  envsubst "${VITE_API_URL}" < /usr/share/nginx/html/env.js.tpl > /usr/share/nginx/html/env.js
+  # Substitui a variável ${VITE_API_URL} (ou $VITE_API_URL) no template
+  envsubst '${VITE_API_URL}' < /usr/share/nginx/html/env.js.tpl > /usr/share/nginx/html/env.js
 fi
